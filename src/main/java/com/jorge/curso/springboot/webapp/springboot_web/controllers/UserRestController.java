cfccd267ae7a35d663879a0914e53469a69dac9f
arq.jorgedelira@gmail.com <e-jedelira@neoris.com>
2024-10-03 12:56:37 -0600

    Actualización de proyecto Spring Boot 10/03/2024

---
+++ src/main/java/com/jorge/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -0,0 +1,21 @@
+package com.jorge.curso.springboot.webapp.springboot_web.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestController {
+    
+    @GetMapping("/details2")
+    public Map<String, Object> details() {
+        Map<String, Object> body = new HashMap<>();
+        body.put("title", "Hola Mundo Spring Boot");
+        body.put("name", "Jorge");
+        body.put("lastname", "De Lira");
+
+        return body;
+    }
+}
